06ef340926ce70dbcb1cf3abcd57c66bd6904f7d
DangLiem <dangliem@DangLiem.local>
2020-08-07 18:19:56 +0700

    reup nuxtjs pj

---
+++ neko-app/components/Header/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="navTop fixed-top">
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <nuxt-link to="/">
+            <a class="nav-link">Diễn đàn</a>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Java</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">PHP</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Các khoá học</a>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/about">
+            <a class="nav-link">Giới thiệu</a>
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="slider d-flex flex-column align-items-center justify-content-center">
+      <div class="row d-flex">
+        <div class="col-md-5">
+          <img
+            id="imgBg"
+            src="https://64.media.tumblr.com/tumblr_ma1ur9ch931qdabzno1_1280.png"
+            alt
+            srcset
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navTop {
+  background-color: #f5f7fb;
+}
+#imgBg {
+  width: 100%;
+  height: 100%;
+}
+.nav-links {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ neko-app/pages/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="row">
+    <div class="col-md-9 leftMain">
+      <div class="post mt-2 d-flex flex-column justify-content-center">
+        <div class="title">
+          <h2>Senior hay không-Senior?</h2>
+        </div>
+        <div class="content pt-2">
+          <p>
+            Mình sẽ mở đầu bài viết với một câu châm ngôn của bác Martin Fowler mà mình nghĩ ai là Software Engineer cũng đều nên nhớ, "Any fool can write code that a computer can understand. Good programmers write code that humans can
+            understand." Tạm dịch là, "Bất cứ thằng ngu nào cũng có thể...
+          </p>
+        </div>
+        <div class="btnRead">
+          <button class="btn-primary">Tiếp tục đọc</button>
+        </div>
+        <div class="tag d-flex justify-content-end">
+          <a class="pr-3" href="#">Dang Liem</a>
+          <a class="pr-3" href="#">Java</a>
+          <a class="pr-3" href="#">22 Bình luận</a>
+          <p class="pr-3">02/06/2020</p>
+        </div>
+      </div>
+      <hr />
+      <div class="post mt-2 d-flex flex-column justify-content-center">
+        <div class="title">
+          <h2>Đẳng cấp một dân chơi?</h2>
+        </div>
+        <div class="content pt-2">
+          <p>
+            Mình sẽ mở đầu bài viết với một câu châm ngôn của bác Martin Fowler mà mình nghĩ ai là Software Engineer cũng đều nên nhớ, "Any fool can write code that a computer can understand. Good programmers write code that humans can
+            understand." Tạm dịch là, "Bất cứ thằng ngu nào cũng có thể...
+          </p>
+        </div>
+        <div class="btnRead">
+          <button class="btn-primary">Tiếp tục đọc</button>
+        </div>
+        <div class="tag d-flex justify-content-end">
+          <a class="pr-3" href="#">Kha Banh</a>
+          <a class="pr-3" href="#">Java</a>
+          <a class="pr-3" href="#">999 Bình luận</a>
+          <p class="pr-3">02/06/2020</p>
+        </div>
+      </div>
+      <hr />
+      <div class="post mt-2 d-flex flex-column justify-content-center">
+        <div class="title">
+          <h2>Con gà gáy le té le té</h2>
+        </div>
+        <div class="content pt-2">
+          <p>
+            Mình sẽ mở đầu bài viết với một câu châm ngôn của bác Martin Fowler mà mình nghĩ ai là Software Engineer cũng đều nên nhớ, "Any fool can write code that a computer can understand. Good programmers write code that humans can
+            understand." Tạm dịch là, "Bất cứ thằng ngu nào cũng có thể...
+          </p>
+        </div>
+        <div class="btnRead">
+          <button class="btn-primary">Tiếp tục đọc</button>
+        </div>
+        <div class="tag d-flex justify-content-end">
+          <a class="pr-3" href="#">user</a>
+          <a class="pr-3" href="#">Java</a>
+          <a class="pr-3" href="#">Bình luận</a>
+          <p class="pr-3">02/06/2020</p>
+        </div>
+      </div>
+      <hr />
+      <div class="post mt-2 d-flex flex-column justify-content-center">
+        <div class="title">
+          <h2>Senior hay không-Senior?</h2>
+        </div>
+        <div class="content pt-2">
+          <p>
+            Mình sẽ mở đầu bài viết với một câu châm ngôn của bác Martin Fowler mà mình nghĩ ai là Software Engineer cũng đều nên nhớ, "Any fool can write code that a computer can understand. Good programmers write code that humans can
+            understand." Tạm dịch là, "Bất cứ thằng ngu nào cũng có thể...
+          </p>
+        </div>
+        <div class="btnRead">
+          <button class="btn-primary">Tiếp tục đọc</button>
+        </div>
+        <div class="tag d-flex justify-content-end">
+          <a class="pr-3" href="#">user</a>
+          <a class="pr-3" href="#">Java</a>
+          <a class="pr-3" href="#">Bình luận</a>
+          <p class="pr-3">02/06/2020</p>
+        </div>
+      </div>
+      <hr />
+    </div>
+    <div class="col-md-3 rightMain">
+      <div class="search pt-3">
+        <label for="search">Tìm kiếm</label>
+        <input type="text" id="search" />
+      </div>
+      <div class="viewMost pt-5">
+        <h5>Nhiều người xem</h5>
+        <ul>
+          <li>
+            <a href="#">CORS là gì?</a>
+          </li>
+          <li>
+            <a href="#">69 Câu hỏi phỏng vấn?</a>
+          </li>
+          <li>
+            <a href="#">Spring MVC?</a>
+          </li>
+          <li>
+            <a href="#">Spirng security?</a>
+          </li>
+          <li>
+            <a href="#">JSP trong java ?</a>
+          </li>
+          <li>
+            <a href="#">Stream API?</a>
+          </li>
+          <li>
+            <a href="#">Colection trong java 8?</a>
+          </li>
+        </ul>
+      </div>
+      <div class="viewMost pt-5">
+        <h5>Chủ đề</h5>
+        <ul>
+          <li>
+            <a href="#">CORS là gì?</a>
+          </li>
+          <li>
+            <a href="#">69 Câu hỏi phỏng vấn?</a>
+          </li>
+          <li>
+            <a href="#">Spring MVC?</a>
+          </li>
+          <li>
+            <a href="#">Spirng security?</a>
+          </li>
+          <li>
+            <a href="#">JSP trong java ?</a>
+          </li>
+          <li>
+            <a href="#">Stream API?</a>
+          </li>
+          <li>
+            <a href="#">Colection trong java 8?</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
